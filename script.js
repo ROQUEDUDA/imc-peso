4307a85463f1e0b9f023fe1390081028c18cb6ad
MARIA EDUARDA ROQUE <00001107786277sp@aluno.educacao.sp.gov.br>
2024-06-05 13:05:44 -0300

    novo

---
+++ script.js
@@ -0,0 +1,5 @@
+function calcular(){
+    var fPeso = parseFloat(document.getElementById("peso"))
+    var fAltura = parseFloat(document.getElementById("altura"))
+    var fImc = fPeso /(fAltura*fAltura)
+}
